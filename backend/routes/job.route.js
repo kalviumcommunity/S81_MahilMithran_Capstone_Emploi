1f098b4f24dbda291d3f00d413ed38192f97a177
mahilmithranks <mahilmithranks2007@gmail.com>
2025-05-28 10:23:15 +0530

    done

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,13 +1,13 @@
-                        import express from "express";
-                        import isAuthenticated from "../middlewares/isAuthenticated.js";
-                        import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
+    import express from "express";
+    import isAuthenticated from "../middlewares/isAuthenticated.js";
+    import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
 
-                        const router = express.Router();
+    const router = express.Router();
 
-                        // Routes for jobs
-                        router.post("/jobs", isAuthenticated, postJob);
-                        router.get("/jobs", isAuthenticated, getAllJobs);       
-                        router.get("/admin/jobs", isAuthenticated, getAdminJobs); 
-                        router.get("/jobs/:id", isAuthenticated, getJobById);   
+    // Routes for jobs
+    router.post("/jobs", isAuthenticated, postJob);
+    router.get("/jobs", isAuthenticated, getAllJobs);       
+    router.get("/admin/jobs", isAuthenticated, getAdminJobs); 
+    router.get("/jobs/:id", isAuthenticated, getJobById);   
 
-                        export default router;
+    export default router;
